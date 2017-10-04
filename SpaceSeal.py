e6d71a95706602f3741638902927368a6ce763a9
Ryolenyo <ryolenyo@gmail.com>
2017-10-04 20:11:57 +0700

    seal movement

--- SpaceSeal.py
+++ SpaceSeal.py
@@ -1,17 +1,52 @@
 import arcade
 from random import randint
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+from pyglet.window import key
 
-class SpaceBG(arcade.Window):
-    def __init__(self,width,height):
-            super().__init__(width,height)
+win_w = 800
+win_h = 600
+keys = key.KeyStateHandler()
 
-            arcade.set_background_color(arcade.color.BLACK)
+class Player:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.Seal = arcade.Sprite('images/seal.png')
+        self.Seal.set_position(self.x,self.y)
 
-    def on_draw(self):
+    def draw(self):
         arcade.start_render()
+        self.Seal.draw()
+
+    def move(self,keys):
+        if keys[key.LEFT]:
+            self.x -= 3
+            self.Seal.set_position(self.x,self.y)
+        if keys[key.RIGHT]:
+            self.x += 3
+            self.Seal.set_position(self.x,self.y)
+        if keys[key.UP]:
+            self.y += 3
+            self.Seal.set_position(self.x,self.y)
+        if keys[key.DOWN]:
+            self.y -= 3
+            self.Seal.set_position(self.x,self.y)
+        
+            
+player = Player(400,100)       
+        
+
+def on_draw(delta_time):
+    arcade.start_render()
+    player.move(keys)
+    player.draw()
+    
+    
+def main():
+    arcade.open_window(win_w,win_h,"SPACE SEAL")
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.get_window().push_handlers(keys)
+    arcade.schedule(on_draw,1/80)
+    arcade.run()
 
 if __name__ == '__main__':
-        window = SpaceBG(SCREEN_WIDTH,SCREEN_HEIGHT)
-        arcade.run()
+    main()
